85fefbd7f0628ae330399bd85fdecfbb4135f8bb
Angelina Davies <angelinasuperstar@Angelinas-MacBook.local>
2018-07-22 22:00:53 -0400

    slight formatting

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ app.use(bodyParser.text());
 require("./app/routing/apiRoutes.js")(app); 
 require("./app/routing/htmlRoutes.js")(app);
 
-
-
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
 });
